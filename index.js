c6d5bba6e0d859ac80d063ab1c2b98056eadadf8
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-03-09 01:18:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,84 @@
 const https = require('https');
-const { exec } = require('child_process');
 const fs = require('fs');
+const exec = require('child_process').exec;
+// 安装bash和unzip
+exec('sudo apt-get update && sudo apt-get install -y bash unzip', (err, stdout, stderr) => {
+  if (err) {
+    console.error(`执行命令时出错: ${err}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
 
-const v2rayDownloadUrl = 'https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip';
-const v2rayConfigUrl = 'https://raw.githubusercontent.com/v2fly/v2ray-core/master/release/config/server/config.json';
-const v2rayConfigPath = '/etc/v2ray/config.json';
+// 下载v2ray二进制文件
+const v2rayUrl = 'https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip';
+const v2rayFile = './v2ray-linux-64.zip';
+const v2rayStream = fs.createWriteStream(v2rayFile);
 
-const downloadAndConfigureV2ray = () => {
-  // Download v2ray binary
-  https.get(v2rayDownloadUrl, (res) => {
-    const v2rayZipPath = '/tmp/v2ray.zip';
-    const file = fs.createWriteStream(v2rayZipPath);
-    res.pipe(file);
-    file.on('finish', () => {
-      file.close();
+https.get(v2rayUrl, (res) => {
+  res.pipe(v2rayStream);
 
-      // Extract v2ray binary
-      exec(`unzip ${v2rayZipPath} -d /usr/local/bin`, (err) => {
+  res.on('end', () => {
+    console.log('v2ray binary downloaded');
+
+    // 解压v2ray二进制文件
+    exec(`unzip ${v2rayFile} -d ./v2ray`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`v2ray binary extraction failed: ${err}`);
+        return;
+      }
+
+      console.log('v2ray binary extracted');
+
+      // 写入配置文件
+      const config = {
+    "inbounds": [
+      {
+        "protocol": "vmess",
+        "listen": "0.0.0.0",
+        "port": 8080,
+        "settings": {
+          "clients": [
+            {
+              "id": "b831381d-6324-4d53-ad4f-8cda48b30811",
+              "alterId": 64
+            }
+          ]
+        },
+        "streamSettings": {
+          "network": "ws",
+          "wsSettings": {
+            "path": "/ray272449844"
+          }
+        }
+      }
+    ],
+    "outbounds": [
+      {
+        "protocol": "freedom",
+        "settings": {}
+      }
+    ]
+  };
+
+      fs.writeFile('./v2ray/config.json', JSON.stringify(config), (err) => {
         if (err) {
-          console.error('Failed to extract v2ray binary:', err);
+          console.error(`v2ray config file creation failed: ${err}`);
           return;
         }
 
-        console.log('v2ray binary extracted successfully.');
+        console.log('v2ray config file created');
 
-        // Download v2ray config file
-        https.get(v2rayConfigUrl, (res) => {
-          let configData = '';
-          res.on('data', (chunk) => {
-            configData += chunk;
-          });
-          res.on('end', () => {
-            // Parse v2ray config file as JSON object
-            const config = JSON.parse(configData);
+        // 启动v2ray代理
+        exec('./v2ray/v2ray', (err, stdout, stderr) => {
+          if (err) {
+            console.error(`v2ray proxy start failed: ${err}`);
+            return;
+          }
 
-            // Customize v2ray config as desired
-            config.inbounds[0].port = 443;
-            config.inbounds[0].protocol = 'vmess';
-            config.inbounds[0].settings.clients[0].id = 'b831381d-6324-4d53-ad4f-8cda48b30811';
-            config.inbounds[0].settings.clients[0].alterId = 64;
-
-            // Write customized v2ray config file
-            fs.writeFile(v2rayConfigPath, JSON.stringify(config), (err) => {
-              if (err) {
-                console.error('Failed to write v2ray config file:', err);
-                return;
-              }
-
-              console.log('v2ray config file written successfully.');
-
-              // Restart v2ray service to apply new config
-              exec('systemctl restart v2ray', (err) => {
-                if (err) {
-                  console.error('Failed to restart v2ray service:', err);
-                  return;
-                }
-
-                console.log('v2ray service restarted successfully.');
-              });
-            });
-          });
+          console.log('v2ray proxy started');
         });
       });
     });
   });
-};
-
-downloadAndConfigureV2ray();
+});
